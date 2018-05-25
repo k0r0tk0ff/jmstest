67135f7bb86c9f45af7c7d906a11c9e469f4e329
k0r0tk0ff <korotkov34reg@gmail.com>
2018-05-25 21:35:51 +0300

    add server side

--- src/main/java/ru/k0r0tk0ff/EmbeddedActiveMqServer.java
+++ src/main/java/ru/k0r0tk0ff/EmbeddedActiveMqServer.java
@@ -1,5 +1,30 @@
 package ru.k0r0tk0ff;
 
 
+import org.apache.activemq.broker.BrokerService;
+
 public class EmbeddedActiveMqServer {
+
+    public void startActiveMqServer(BrokerService brokerService) {
+
+    //BrokerService brokerService = new BrokerService();
+        try {
+            brokerService.setPersistent(false);
+            brokerService.setUseJmx(false);
+            brokerService.addConnector("tcp://localhost:61616");
+            brokerService.start();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stopActiveMqServer(BrokerService brokerService) {
+        try {
+            brokerService.stop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/main/java/ru/k0r0tk0ff/Main.java
+++ src/main/java/ru/k0r0tk0ff/Main.java
@@ -1,8 +1,22 @@
 package ru.k0r0tk0ff;
 
+import org.apache.activemq.broker.BrokerService;
+
 public class Main {
 
     public static void main(String[]args){
+       // Sender sender = new Sender();
+       // Receiver receiver = new Receiver();
+        EmbeddedActiveMqServer activeMqServer = new EmbeddedActiveMqServer();
+        BrokerService brokerService = new BrokerService();
+
+        activeMqServer.startActiveMqServer(brokerService);
+
+      //  sender.sendMessage();
+
+      //  receiver.receiveMessage();
+
+        activeMqServer.startActiveMqServer(brokerService);
 
     }
 
