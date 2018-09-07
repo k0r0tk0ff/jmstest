fe365516bd8715c89dffbfd7aebef2dce4109b79
Alexandr <korotkov34reg@gmail.com>
2018-09-07 16:52:20 +0300

    Update Main.java

--- src/main/java/ru/k0r0tk0ff/Main.java
+++ src/main/java/ru/k0r0tk0ff/Main.java
@@ -4,49 +4,44 @@ import org.apache.activemq.broker.BrokerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.jms.Session;
+
 public class Main {
 
     private static final Logger LOG  = LoggerFactory.getLogger(Main.class);
+    private static final boolean ISPERSISTANT = true;
+    private static final boolean ISTRANSACTED = true;
+    private static final String URL = "tcp://localhost:61616";
+    private static final String QUEUENAME = "queue";
+
+    //private static final int MODE = Session.SESSION_TRANSACTED;
+    //private static final int MODE = Session.AUTO_ACKNOWLEDGE;
+    //private static final int MODE = Session.DUPS_OK_ACKNOWLEDGE;
+    private static final int MODE = Session.CLIENT_ACKNOWLEDGE;
 
     public static void main(String[]args){
 
-        if(LOG.isDebugEnabled()) {
-            LOG.debug("Start application jmstest ..............");
-        }
+        LOG.info("Start application jmstest ..............");
+        LOG.info("MODE = CLIENT_ACKNOWLEDGE");
 
-        Sender sender = new Sender();
-
-        if(LOG.isDebugEnabled()) {
-            LOG.debug("Success create sender object........");
-        }
+        Sender sender = new Sender(QUEUENAME);
+        LOG.info("Success create sender object........");
         
-        Receiver receiver = new Receiver();
-
-        if(LOG.isDebugEnabled()) {
-            LOG.debug("Success create receiver object ........");
-        }
+        Receiver receiver = new Receiver(QUEUENAME);
+        LOG.info("Success create receiver object ........");
 
         BrokerService brokerService = new BrokerService();
         try {
-            brokerService.setPersistent(false);
+            brokerService.setPersistent(ISPERSISTANT);
             brokerService.setUseJmx(false);
-            brokerService.addConnector("tcp://localhost:61616");
-
+            brokerService.addConnector(URL);
+            System.out.println(brokerService.isPersistent());
             brokerService.start();
-
-            if(LOG.isDebugEnabled()) {
-                LOG.debug("Success start BrokerService ........");
-            }
-            
-            sender.sendMessage();
-
-            receiver.receiveMessage();
-
+            LOG.info("Success start BrokerService ........");
+            sender.sendMessage(MODE, ISTRANSACTED, URL);
+            receiver.receiveMessage(MODE, ISTRANSACTED, URL);
             brokerService.stop();
-
-            if(LOG.isDebugEnabled()) {
-                LOG.debug("Success stop BrokerService ........");
-            }
+            LOG.info("Success stop BrokerService ........");
         } catch (Exception e) {
             LOG.error(".......................................................................");
             LOG.error(e.toString());
